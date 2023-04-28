48349533a0ef972c48dc0b22060fdec1c841fa86
Sasha867 <aleksandrlozicnyj@gmail.com>
2023-04-28 21:50:38 +0300

    fix: reinstall eslint and feat: add keysArray

---
+++ scripts.js
@@ -0,0 +1,285 @@
+const keysArray = [
+  {
+    valueRu: 'ё',
+    valueEng: '`',
+    key: 'Backquote',
+  },
+  {
+    value: '1',
+    specialValue: '!',
+    key: 'Digit1',
+  },
+  {
+    value: '2',
+    specialValue: '@',
+    key: 'Digit2',
+  },
+  {
+    value: '3',
+    specialValue: '#',
+    key: 'Digit3',
+  },
+  {
+    value: '4',
+    specialValue: '$',
+    key: 'Digit4',
+  },
+  {
+    value: '5',
+    specialValue: '%',
+    key: 'Digit5',
+  },
+  {
+    value: '6',
+    specialValue: '^',
+    key: 'Digit6',
+  },
+  {
+    value: '7',
+    specialValue: '&',
+    key: 'Digit7',
+  },
+  {
+    value: '8',
+    specialValue: '*',
+    key: 'Digit8',
+  },
+  {
+    value: '9',
+    specialValue: '(',
+    key: 'Digit9',
+  },
+  {
+    value: '0',
+    specialValue: ')',
+    key: 'Digit0',
+  },
+  {
+    value: '-',
+    specialValue: '_',
+    key: 'Minus',
+  },
+  {
+    value: '=',
+    specialValue: '+',
+    key: 'Equal',
+  },
+  {
+    value: 'Backspace',
+    key: 'Backspace',
+  },
+  {
+    value: 'Tab',
+    key: 'Tab',
+  },
+  {
+    valueRu: 'й',
+    valueEng: 'q',
+    key: 'KeyQ',
+  },
+  {
+    valueRu: 'ц',
+    valueEng: 'w',
+    key: 'KeyW',
+  },
+  {
+    valueRu: 'у',
+    valueEng: 'e',
+    key: 'KeyE',
+  },
+  {
+    valueRu: 'к',
+    valueEng: 'r',
+    key: 'KeyR',
+  },
+  {
+    valueRu: 'е',
+    valueEng: 't',
+    key: 'KeyT',
+  },
+  {
+    valueRu: 'н',
+    valueEng: 'y',
+    key: 'KeyY',
+  },
+  {
+    valueRu: 'г',
+    valueEng: 'u',
+    key: 'KeyU',
+  },
+  {
+    valueRu: 'ш',
+    valueEng: 'i',
+    key: 'KeyI',
+  },
+  {
+    valueRu: 'щ',
+    valueEng: 'o',
+    key: 'KeyO',
+  },
+  {
+    valueRu: 'з',
+    valueEng: 'p',
+    key: 'KeyP',
+  },
+  {
+    valueRu: 'х',
+    valueEng: '[',
+    key: 'BracketLeft',
+  },
+  {
+    valueRu: 'ъ',
+    valueEng: ']',
+    key: 'BracketRight',
+  },
+  {
+    valueRu: '\\',
+    valueEng: '/',
+    key: 'Backslash',
+  },
+  {
+    value: 'Caps lock',
+    key: 'CapsLock',
+  },
+  {
+    valueRu: 'ф',
+    valueEng: 'a',
+    key: 'KeyA',
+  },
+  {
+    valueRu: 'ы',
+    valueEng: 's',
+    key: 'KeyS',
+  },
+  {
+    valueRu: 'в',
+    valueEng: 'd',
+    key: 'KeyD',
+  },
+  {
+    valueRu: 'а',
+    valueEng: 'f',
+    key: 'KeyF',
+  },
+  {
+    valueRu: 'п',
+    valueEng: 'g',
+    key: 'KeyG',
+  },
+  {
+    valueRu: 'р',
+    valueEng: 'h',
+    key: 'KeyH',
+  },
+  {
+    valueRu: 'о',
+    valueEng: 'j',
+    key: 'KeyJ',
+  },
+  {
+    valueRu: 'л',
+    valueEng: 'k',
+    key: 'KeyK',
+  },
+  {
+    valueRu: 'д',
+    valueEng: 'l',
+    key: 'KeyL',
+  },
+  {
+    valueRu: 'ж',
+    valueEng: ';',
+    key: 'Semicolon',
+  },
+  {
+    valueRu: 'э',
+    valueEng: "'",
+    key: 'Quote',
+  },
+  {
+    value: 'Enter',
+    key: 'Enter',
+  },
+  {
+    value: 'Shift',
+    key: 'ShiftLeft',
+  },
+  {
+    valueRu: 'я',
+    valueEng: 'z',
+    key: 'KeyZ',
+  },
+  {
+    valueRu: 'ч',
+    valueEng: 'x',
+    key: 'KeyX',
+  },
+  {
+    valueRu: 'c',
+    valueEng: 'с',
+    key: 'KeyC',
+  },
+  {
+    valueRu: 'м',
+    valueEng: 'v',
+    key: 'KeyV',
+  },
+  {
+    valueRu: 'и',
+    valueEng: 'b',
+    key: 'KeyB',
+  },
+  {
+    valueRu: 'т',
+    valueEng: 'n',
+    key: 'KeyN',
+  },
+  {
+    valueRu: 'ь',
+    valueEng: 'm',
+    key: 'KeyM',
+  },
+  {
+    valueRu: 'б',
+    valueEng: ',',
+    key: 'Comma',
+  },
+  {
+    valueRu: 'ю',
+    valueEng: '.',
+    key: 'Period',
+  },
+  {
+    valueRu: '.',
+    valueEng: '/',
+    key: 'Slash',
+  },
+  {
+    value: 'Shift',
+    key: 'ShiftRight',
+  },
+  {
+    value: 'Ctrl',
+    key: 'ControlLeft',
+  },
+  {
+    value: 'Win',
+    key: 'MetaLeft',
+  },
+  {
+    value: 'Alt',
+    key: 'AltLeft',
+  },
+  {
+    value: 'Space',
+    key: 'Space',
+  },
+  {
+    value: 'Alt',
+    key: 'AltRight',
+  },
+  {
+    value: 'Ctrl',
+    key: 'ControlRight',
+  },
+];
